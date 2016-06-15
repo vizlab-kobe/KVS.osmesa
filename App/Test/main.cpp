a1b3abbfbdfcd294b19c940ccb61de80e4414648
naohisas <naohisa.sakamoto@gmail.com>
2016-06-15 19:51:11 +0900

    Modified.

--- App/Test/main.cpp
+++ App/Test/main.cpp
@@ -1,57 +1,40 @@
 #include <iostream>
-#include "OSMesa.h"
-#include "Screen.h"
-
+#include <sstream>
+#include <iomanip>
+#include <KVS.osmesa/Lib/OSMesa.h>
+#include <KVS.osmesa/Lib/Screen.h>
 #include <kvs/StructuredVolumeObject>
 #include <kvs/StructuredVolumeImporter>
-#include <kvs/PolygonObject>
-#include <kvs/PolygonRenderer>
-#include <kvs/Isosurface>
 #include <kvs/HydrogenVolumeData>
 #include <kvs/RayCastingRenderer>
-#include <kvs/PointObject>
-#include <kvs/CellByCellUniformSampling>
-#include <kvs/ParticleBasedRenderer>
-#include <kvs/TransferFunction>
 
 
 int main( int argc, char** argv )
 {
-    std::cout << "OSMesa Version: " << kvs::osmesa::Version() << std::endl;
+    std::cout << "OSMesa version: " << kvs::osmesa::Version() << std::endl;
 
     kvs::StructuredVolumeObject* volume = NULL;
     if ( argc > 1 ) volume = new kvs::StructuredVolumeImporter( std::string( argv[1] ) );
-    else            volume = new kvs::HydrogenVolumeData( kvs::Vector3ui( 64, 64, 64 ) );
+    else            volume = new kvs::HydrogenVolumeData( kvs::Vec3u( 64, 64, 64 ) );
 
-/*
-    const double i = ( volume->maxValue() + volume->minValue() ) * 0.5;
-    const kvs::PolygonObject::NormalType n = kvs::PolygonObject::VertexNormal;
-    const bool d = false;
-    const kvs::TransferFunction t( 256 );
-    kvs::PolygonObject* object = new kvs::Isosurface( volume, i, n, d, t );
-    delete volume;
-
-    // kvs::glsl::PolygonRenderer* renderer = new kvs::glsl::PolygonRenderer();
-    kvs::PolygonRenderer* renderer = new kvs::PolygonRenderer();
-*/
     kvs::StructuredVolumeObject* object = volume;
-//    kvs::glsl::RayCastingRenderer* renderer = new kvs::glsl::RayCastingRenderer();
     kvs::RayCastingRenderer* renderer = new kvs::RayCastingRenderer();
-/*
-    kvs::TransferFunction tfunc( 256 );
-    kvs::PointObject* object = new kvs::CellByCellUniformSampling( volume, 2, 0.5, tfunc );
-//    kvs::ParticleBasedRenderer* renderer = new kvs::ParticleBasedRenderer();
-    kvs::glsl::ParticleBasedRenderer* renderer = new kvs::glsl::ParticleBasedRenderer();
-    renderer->setRepetitionLevel( 4 );
-*/
+
     kvs::osmesa::Screen screen;
     screen.setGeometry( 0, 0, 512, 512 );
     screen.registerObject( object, renderer );
 
-    object->multiplyXform( kvs::Xform::Rotation( kvs::Mat3::RotationY( 30 ) ) );
+    for ( size_t i = 0; i < 12; i++ )
+    {
+        std::stringstream num; num << std::setw(3) << std::setfill('0') << i;
+        std::string filename = "output_" + num.str() + ".bmp";
 
-    screen.draw();
-    screen.capture().write("output.bmp");
+        std::cout << "rendering to ... " << std::flush;
+        object->multiplyXform( kvs::Xform::Rotation( kvs::Mat3::RotationY( 30 ) ) );
+        screen.draw();
+        screen.capture().write( filename );
+        std::cout << filename << std::endl;
+    }
 
     return 0;
 }

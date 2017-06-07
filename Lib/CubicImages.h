2951cabf478276b2f8aef4bbbd0896231ab59ac0
Naohisa Sakamoto <naohisa@SKMT-MacBookPro13TB.local>
2017-06-07 20:13:44 +0900

    Modified.

--- Lib/CubicImages.h
+++ Lib/CubicImages.h
@@ -37,6 +37,15 @@ public:
     const kvs::ColorImage& bottomImage() const { return this->image( Bottom ); }
     const kvs::ColorImage& backImage() const { return this->image( Back ); }
     const kvs::ColorImage& frontImage() const { return this->image( Front ); }
+
+    void setImage( const Direction& dir, const kvs::ColorImage& image ) { m_images[dir] = image; }
+    void setRightImage( const kvs::ColorImage& image ) { this->setImage( Right, image ); }
+    void setLeftImage( const kvs::ColorImage& image ) { this->setImage( Left, image ); }
+    void setTopImage( const kvs::ColorImage& image ) { this->setImage( Top, image ); }
+    void setBottomImage( const kvs::ColorImage& image ) { this->setImage( Bottom, image ); }
+    void setBackImage( const kvs::ColorImage& image ) { this->setImage( Back, image ); }
+    void setFrontImage( const kvs::ColorImage& image ) { this->setImage( Front, image ); }
+
     const kvs::ColorImage cubeMapImage() const;
     const kvs::ColorImage sphericalMapImage() const;
     void draw( kvs::osmesa::Screen& screen, const kvs::Vec3 p );
